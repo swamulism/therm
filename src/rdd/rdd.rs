b5cf228bc150eb09203c8462ca5a23013b2172bb
samuel <swamulism@gmail.com>
2020-07-11 00:18:40 -0500

    Change RDD and RDDPair (no progress)
    Co-authored-by: Patrick Liu <ptliu@users.noreply.github.com>

--- src/rdd/rdd.rs
+++ src/rdd/rdd.rs
@@ -7,31 +7,30 @@ use crate::dependency::Dependency;
 
 pub(crate) struct RddVals {
     pub id: usize,
-    pub dependencies: Vec<dyn Dependency>,
+    // pub dependencies: Vec<dyn Dependency>,
     should_cache: bool,
     pub context: Context,
 }
 
 trait RDD<T> {
-    
+    type Item;
     //Transformations
-    fn filter(&self, f: fn(T) -> bool) -> dyn RDD<T>;
-    fn map<U>(&self, f: fn(T) -> U) -> dyn RDD<U>;
+    fn filter(&self, f: fn(T) -> bool) -> Box<dyn RDD<T, Item=Self::Item>>;
+    //fn map<T,U>(&self, f: fn(T) -> U) -> Box<dyn RDD>;
 
     //Actions
-    fn reduce(f: fn(T, T) -> T) -> T;
-    fn save(path: String);
+    fn reduce(&self, f: fn(T, T) -> T) -> T;
+    fn save(&self, path: String);
 
-    fn saveAsTextFile(path: String);
+    fn saveAsTextFile(&self, path: String);
     //def map[U: ClassTag](f: T => U): RDD[U]
 }
 
-trait RDDPair<K, V>: RDD<(K,V)> {
-    fn groupByKey<T>() -> dyn RDDPair<K, dyn T>
-    where 
-        T: IntoIterator,
-        T::Item:V,;
-    fn reduceByKey(f: fn(V, V) -> V) -> dyn RDDPair<K, V>;
+trait RDDPair<K, V>: RDD<(K, V)> {
+    fn groupByKey(&self) -> dyn RDD<K, Item=Self::Item>;
+    // where 
+        // T: IntoIterator<Item=V>,;
+    fn reduceByKey(&self, f: fn(V, V) -> V) -> dyn RDDPair<K,V, Item=Self::Item>;
 
-    fn sort(c: fn(K, K) -> Ordering) -> dyn RDDPair<K, V>;
+    fn sort(&self, c: fn(K, K) -> Ordering) -> dyn RDDPair<K,V, Item=Self::Item>;
 }
